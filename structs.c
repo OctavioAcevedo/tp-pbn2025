82ed3d752afbc30d264c99407cd6c42ac574555c
Octavio Acevedo <octavioaacvedo@gmail.com>
2025-06-16 16:28:02 -0300

    listar y modificar estudiantes y materias, listar estudiantes por edad y nombre o apellido

--- structs.c
+++ structs.c
@@ -35,16 +35,31 @@ typedef struct inscripcion
 Estudiante *legajo1 = NULL;
 Materia *materia1 = NULL;
 
+void limpiarTerminal();
+
+// Estudiantes
 void altaEstudiante(char *nombre, char *apellido, int edad);
-void altaMateria(char *nombre);
-void inscribirMateria(int legajo, char *nombreMateria);
+void listarEstudiantes();
+void modificarEstudiante(int legajo, char *nombre, char *apellido, int edad);
+void listarPorNombreApellido(char *nombreApellido);
+void listarPorEdad(int edadMin, int edadMax);
 Estudiante *buscarPorLegajo(int legajo);
+
+// Materias
+void altaMateria(char *nombre);
+void listarMaterias();
+void modificarMateria(char *nombre, char *nuevoNombre);
 Materia *buscarMateria(char *nombreMateria);
+
+// Inscripciones
+void inscribirMateria(int legajo, char *nombreMateria);
 void listarMateriasPorEstudiante(int legajo);
 void listarEstudiantesPorMateria(char *nombreMateria);
 
 int main()
 {
+    limpiarTerminal();
+
     altaEstudiante("Juan", "Perez", 20);
     altaEstudiante("Maria", "Gomez", 22);
     altaEstudiante("Pedro", "Lopez", 21);
@@ -67,10 +82,15 @@ int main()
     inscribirMateria(1, "Diseño Lógico");
     inscribirMateria(2, "Programación bajo nivel");
 
-    while(legajo1){
-        listarMateriasPorEstudiante(legajo1->legajo);
-        legajo1 = legajo1->next;
-    }
+    listarEstudiantes();
+    listarMaterias();
+
+    listarPorNombreApellido("Juan");
+    listarPorNombreApellido("-");
+    listarPorEdad(20, 22);
+    listarPorEdad(17, 18);
+    listarPorEdad(17, 10);
+
     return 0;
 }
 
@@ -104,6 +124,153 @@ void altaEstudiante(char *nombre, char *apellido, int edad)
         nuevo->legajo = actual->legajo + 1;
     }
 }
+
+void listarEstudiantes()
+{
+    Estudiante *actual = legajo1;
+    while (actual)
+    {
+        printf("Legajo: %d, Nombre: %s %s, Edad: %d\n", actual->legajo, actual->nombre, actual->apellido, actual->edad);
+        actual = actual->next;
+    }
+}
+
+void modificarEstudiante(int legajo, char *nombre, char *apellido, int edad)
+{
+    Estudiante *estudiante = buscarPorLegajo(legajo);
+    if (estudiante)
+    {
+        estudiante->nombre = nombre;
+        estudiante->apellido = apellido;
+        estudiante->edad = edad;
+    }
+}
+
+void listarPorNombreApellido(char *nombreApellido)
+{
+    Estudiante *actual = legajo1;
+    int encontrado = 0;
+    while (actual)
+    {
+        if (strcmp(actual->nombre, nombreApellido) == 0 || strcmp(actual->apellido, nombreApellido) == 0)
+        {
+            encontrado = 1;
+            printf("Estudiante encontrado: Legajo: %d, Nombre: %s %s, Edad: %d\n", actual->legajo, actual->nombre, actual->apellido, actual->edad);
+        }
+        actual = actual->next;
+    }
+
+    if (encontrado == 0)
+    {
+        printf("No se encontro el estudiante para la busqueda %s\n", nombreApellido);
+    }
+    return;
+}
+
+void listarPorEdad(int edadMin, int edadMax)
+{
+    if (edadMin > edadMax)
+    {
+        printf("El rango de edad es invalido.\n");
+        return;
+    }
+
+    Estudiante *actual = legajo1;
+    int encontrado = 0;
+    while (actual)
+    {
+        if (actual->edad >= edadMin && actual->edad <= edadMax)
+        {
+            encontrado = 1;
+            printf("Estudiante encontrado: Legajo: %d, Nombre: %s %s, Edad: %d\n", actual->legajo, actual->nombre, actual->apellido, actual->edad);
+        }
+        actual = actual->next;
+    }
+
+    if (encontrado == 0)
+    {
+        printf("No se encontraron estudiantes dentro del rango de edad %d - %d \n", edadMin, edadMax);
+    }
+}
+
+Estudiante *buscarPorLegajo(int legajo)
+{
+    Estudiante *actual = legajo1;
+    while (actual)
+    {
+        if (actual->legajo == legajo)
+        {
+            return actual;
+        }
+        if (actual->legajo > legajo)
+        {
+            break;
+        }
+        actual = actual->next;
+    }
+    printf("No se encontro el estudiante con legajo %d\n", legajo);
+    return NULL;
+};
+
+void altaMateria(char *nombre)
+{
+    Materia *nuevaMateria;
+    nuevaMateria = (Materia *)malloc(sizeof(Materia));
+
+    nuevaMateria->nombre = nombre;
+    nuevaMateria->inscripcionesHead = NULL;
+    nuevaMateria->next = NULL;
+    nuevaMateria->prev = NULL;
+
+    if (!materia1)
+    {
+        materia1 = nuevaMateria;
+    }
+    else
+    {
+        Materia *actual = materia1;
+        while (actual->next)
+        {
+            actual = actual->next;
+        }
+        actual->next = nuevaMateria;
+        nuevaMateria->prev = actual;
+    }
+}
+
+void listarMaterias()
+{
+    Materia *actual = materia1;
+    while (actual)
+    {
+        printf("Materia: %s\n", actual->nombre);
+        actual = actual->next;
+    }
+}
+
+void modificarMateria(char *nombre, char *nuevoNombre)
+{
+    Materia *materia = buscarMateria(nombre);
+    if (materia)
+    {
+        materia->nombre = nuevoNombre;
+    }
+}
+
+Materia *buscarMateria(char *nombreMateria)
+{
+    Materia *actual = materia1;
+    while (actual)
+    {
+        if (strcmp(actual->nombre, nombreMateria) == 0)
+        {
+            return actual;
+        }
+        actual = actual->next;
+    }
+    printf("No se encontro la materia %s\n", nombreMateria);
+    return NULL;
+};
 
 void inscribirMateria(int legajo, char *nombreMateria)
 {
@@ -155,67 +322,6 @@ void inscribirMateria(int legajo, char *nombreMateria)
     }
 }
 
-void altaMateria(char *nombre)
-{
-    Materia *nuevaMateria;
-    nuevaMateria = (Materia *)malloc(sizeof(Materia));
-
-    nuevaMateria->nombre = nombre;
-    nuevaMateria->inscripcionesHead = NULL;
-    nuevaMateria->next = NULL;
-    nuevaMateria->prev = NULL;
-
-    if (!materia1)
-    {
-        materia1 = nuevaMateria;
-    }
-    else
-    {
-        Materia *actual = materia1;
-        while (actual->next)
-        {
-            actual = actual->next;
-        }
-        actual->next = nuevaMateria;
-        nuevaMateria->prev = actual;
-    }
-}
-
-//cambiar por busqueda binaria o alguna otra mas performante (los legajos estan ordenados)
-Estudiante *buscarPorLegajo(int legajo)
-{
-    Estudiante *actual = legajo1;
-    while (actual)
-    {
-        if (actual->legajo == legajo)
-        {
-            return actual;
-        }
-        if (actual->legajo > legajo)
-        {
-            break;
-        }
-        actual = actual->next;
-    }
-    printf("No se encontro el estudiante con legajo %d\n", legajo);
-    return NULL;
-};
-
-Materia *buscarMateria(char *nombreMateria)
-{
-    Materia *actual = materia1;
-    while (actual)
-    {
-        if (strcmp(actual->nombre, nombreMateria) == 0)
-        {
-            return actual;
-        }
-        actual = actual->next;
-    }
-    printf("No se encontro la materia %s\n", nombreMateria);
-    return NULL;
-};
-
 void listarMateriasPorEstudiante(int legajo)
 {
     Estudiante *estudiante = buscarPorLegajo(legajo);
@@ -252,3 +358,8 @@ void listarEstudiantesPorMateria(char *nombreMateria)
         }
     }
 }
+
+void limpiarTerminal()
+{
+    printf("\e[1;1H\e[2J");
+};
